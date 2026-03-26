b9172a45b6f275b9db6a572ee2a4052cdfcb54ce
Rahul Makwana <rahulm.octal8@gmail.com>
2026-03-26 12:35:16 +0530

    remove fontstore

--- storefront/src/shopify.js
+++
@@ -1,6 +0,0 @@
-import Client from 'shopify-buy';
-
-export const client = Client.buildClient({
-  domain: import.meta.env.VITE_SHOPIFY_DOMAIN || 'your-shop-name.myshopify.com',
-  storefrontAccessToken: import.meta.env.VITE_SHOPIFY_STOREFRONT_TOKEN || 'your-storefront-api-token'
-});
